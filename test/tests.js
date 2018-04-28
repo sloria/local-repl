728e9ffad2d8af96b052437aa20b280f9168da1d
Steven Loria <sloria1@gmail.com>
2018-04-28 15:28:00 -0400

    Switch to ava

--- test/tests.js
+++ test/tests.js
@@ -1,261 +1,178 @@
-const path = require('path');
-const expect = require('code').expect;
-const Lab = require('lab');
-const p = require('..');
+import * as path from 'path';
+import test from 'ava';
 
-const lab = (exports.lab = Lab.script());
-const describe = lab.describe;
-const it = lab.it;
+import * as p from '..';
 
-describe('getDefaultPrompt', () => {
-  it('should use short names', done => {
-    expect(p.getDefaultPrompt('foo')).to.equal('[foo] > ');
-    done();
-  });
-
-  it('should truncate long names', done => {
-    const name = 'foobarbazquuuux';
-    expect(p.getDefaultPrompt(name)).to.equal('[f] > ');
-    done();
-  });
+test('getDefaultPrompt: should use short names', t => {
+  t.is(p.getDefaultPrompt('foo'), '[foo] > ');
 });
 
-describe('loadContext', () => {
-  it('should load values', done => {
-    p.loadContext([{name: 'foo', value: 42}]).then(context => {
-      expect(context).to.equal({foo: 42});
-      done();
-    });
-  });
-
-  it('should load installed modules', done => {
-    p.loadContext([{name: 'foo', module: 'lodash'}]).then(context => {
-      expect(context.foo).to.be.a.function();
-      done();
-    });
-  });
-
-  it('should load local modules', done => {
-    p
-      .loadContext([{name: 'foo', module: './test/local-module'}])
-      .then(context => {
-        expect(context.foo).to.equal('TEST');
-        done();
-      });
-  });
-
-  it('should throw an error if both module and value passed', done => {
-    p.loadContext([{name: 'foo', value: 42, module: 'lodash'}]).catch(err => {
-      expect(err.message).to.equal(
-        'Context entry for "foo" cannot define both "module" and "value".'
-      );
-      done();
-    });
-  });
-
-  it('should throw an error if neither module nor value are passed', done => {
-    p.loadContext([{name: 'foo'}]).catch(err => {
-      expect(err.message).to.equal(
-        'Context entry must contain either "module" or "value".'
-      );
-      done();
-    });
-  });
-
-  it('should load strings as modules', done => {
-    p.loadContext(['lodash']).then(context => {
-      expect(context.lodash).to.be.a.function();
-      done();
-    });
-  });
-
-  it('should camelcase local module names', done => {
-    p.loadContext(['./test/local-module']).then(context => {
-      expect(context).to.include('localModule');
-      expect(context.localModule).to.equal('TEST');
-      done();
-    });
-  });
-
-  it('should error if name not provided', done => {
-    p.loadContext([{value: 42}]).catch(err => {
-      expect(err.message).to.equal(
-        '"name" is required for each context entry.'
-      );
-      done();
-    });
-  });
-
-  it('should not accept ./', done => {
-    p.loadContext(['./']).catch(err => {
-      expect(err.message).to.equal('Invalid name "./"');
-      done();
-    });
-  });
-
-  it('should load key value pairs', done => {
-    p.loadContext({foo: 42}).then(context => {
-      expect(context).to.equal({foo: 42});
-      done();
-    });
-  });
-
-  it('should load promises with context array', done => {
-    const promise = Promise.resolve(42);
-    p.loadContext([{name: 'meaning', value: promise}]).then(context => {
-      expect(context).to.equal({meaning: 42});
-      done();
-    });
-  });
-
-  it('should load promises with context object', done => {
-    const promise = Promise.resolve(42);
-    p.loadContext({meaning: promise}).then(context => {
-      expect(context).to.equal({meaning: 42});
-      done();
-    });
-  });
-
-  it('should handle rejected promises', done => {
-    const promise = new Promise((resolve, reject) => {
-      reject(new Error('ERROR'));
-    });
-    p.loadContext({err: promise}).catch(err => {
-      expect(err.message).to.equal('ERROR');
-      done();
-    });
-  });
+test('getDefaultPrompt: should truncate long names', t => {
+  const name = 'foobarbazquuuux';
+  t.is(p.getDefaultPrompt(name), '[f] > ');
 });
 
-describe('loadConfiguration', () => {
-  it('should load config from package.json', done => {
-    p
-      .loadConfiguration({
-        package: path.join(__dirname, 'pkg.json'),
-        replrc: false,
-      })
-      .then(result => {
-        expect(result.context).to.equal({bar: 42});
-        expect(result.prompt).to.equal('<TEST> $');
-        expect(result.bannerFunc).to.be.a.function();
-        done();
-      });
-  });
-
-  it('should allow "repl" config to be an array in package.json', done => {
-    p
-      .loadConfiguration({
-        package: path.join(__dirname, 'pkg-with-repl-array.json'),
-        replrc: false,
-      })
-      .then(result => {
-        expect(result.context.bar).to.equal(42);
-        expect(result.context.lodash).to.be.a.function();
-        expect(result.prompt).to.equal('[foo] > ');
-        done();
-      });
-  });
-
-  it('should allow "repl" config to be an array in replrc', done => {
-    p
-      .loadConfiguration({
-        package: false,
-        replrc: path.join(__dirname, 'replrc-with-array.js'),
-      })
-      .then(result => {
-        expect(result.context.bar).to.equal(43);
-        expect(result.context.lodash).to.be.a.function();
-        done();
-      });
-  });
-
-  it('should load config from a replrc file', done => {
-    p
-      .loadConfiguration({
-        package: false,
-        replrc: path.join(__dirname, 'replrc.js'),
-      })
-      .then(result => {
-        expect(result.context.bar).to.equal(43);
-        expect(result.context.lodash).to.be.a.function();
-        expect(result.prompt).to.equal('[TEST] $');
-        done();
-      });
-  });
-
-  it('should give precedence to replrc file', done => {
-    p
-      .loadConfiguration({
-        package: path.join(__dirname, 'pkg.json'),
-        replrc: path.join(__dirname, 'replrc.js'),
-      })
-      .then(result => {
-        expect(result.context.bar).to.equal(43);
-        const banner = result.bannerFunc();
-        expect(banner).to.equal('TEST');
-        done();
-      });
-  });
-
-  it('should allow prompts to be defined as a function', done => {
-    p
-      .loadConfiguration({
-        package: path.join(__dirname, 'pkg.json'),
-        replrc: path.join(__dirname, 'replrc-with-prompt-func.js'),
-      })
-      .then(result => {
-        expect(result.context.foo).to.equal('TEST');
-        expect(result.promptFunc).to.be.a.function();
-        expect(result.promptFunc(result.context, result.package)).to.equal(
-          'TEST foo > '
-        );
-        done();
-      });
-  });
-
-  it('should allow context to be an object', done => {
-    p
-      .loadConfiguration({
-        package: path.join(__dirname, 'pkg.json'),
-        replrc: path.join(__dirname, 'replrc-with-context-obj.js'),
-      })
-      .then(result => {
-        expect(result.context.l).to.be.a.function();
-        expect(result.context.meaningOfLife).to.equal(42);
-        done();
-      });
-  });
-
-  it('should give precedence to passed options', done => {
-    p
-      .loadConfiguration({
-        package: path.join(__dirname, 'pkg.json'),
-        replrc: path.join(__dirname, 'replrc.js'),
-        prompt: 'override > ',
-        banner: 'OVERRIDE',
-      })
-      .then(result => {
-        expect(result.prompt).to.equal('override > ');
-        expect(result.banner).to.equal('OVERRIDE');
-        done();
-      });
-  });
+test('loadContext: should load values', async t => {
+  const context = await p.loadContext([{name: 'foo', value: 42}]);
+  t.deepEqual(context, {foo: 42});
 });
 
-describe('contextKey', () => {
-  it('should camelcase local paths with dashes', done => {
-    expect(p.contextKey('./utils/foo-bar')).to.equal('fooBar');
-    expect(p.contextKey('/utils/foo-bar')).to.equal('fooBar');
-    done();
-  });
+test('loadContext: should load installed modules', async t => {
+  const context = await p.loadContext([{name: 'foo', module: 'lodash'}]);
+  t.true(typeof context.foo === 'function');
+});
 
-  it('should camelcase snakecase names', done => {
-    expect(p.contextKey('./utils/foo_bar')).to.equal('fooBar');
-    done();
-  });
+test('loadContext: should load local modules', async t => {
+  const context = await p.loadContext([{name: 'foo', module: './test/local-module'}]);
+  t.is(context.foo, 'TEST');
+});
 
-  it('should camelcase package names with dashes', done => {
-    expect(p.contextKey('foo-bar')).to.equal('fooBar');
-    done();
+test('loadContext: should throw an error if both module and value passed', async t => {
+  const error = await t.throws(p.loadContext([{name: 'foo', value: 42, module: 'lodash'}]));
+  t.is(error.message, 'Context entry for "foo" cannot define both "module" and "value".');
+});
+
+test('loadContext: should throw an error if neither module nor value are passed', async t => {
+  const error = await t.throws(p.loadContext([{name: 'foo'}]));
+  t.is(error.message, 'Context entry must contain either "module" or "value".');
+});
+
+test('loadContext: should load strings as modules', async t => {
+  const context = await p.loadContext(['lodash']);
+  t.true(typeof context.lodash === 'function');
+});
+
+test('loadContext: should camelcase local module names', async t => {
+  const context = await p.loadContext(['./test/local-module']);
+  t.true(Object.prototype.hasOwnProperty.call(context, 'localModule'));
+  t.is(context.localModule, 'TEST');
+});
+
+test('loadContext: should error if name not provided', async t => {
+  const error = await t.throws(p.loadContext([{value: 42}]));
+  t.is(error.message, '"name" is required for each context entry.');
+});
+
+test('loadContext: should not accept ./', async t => {
+  const error = await t.throws(p.loadContext(['./']));
+  t.is(error.message, 'Invalid name "./"');
+});
+
+test('loadContext: should load key value pairs', async t => {
+  const context = await p.loadContext({foo: 42});
+  t.deepEqual(context, {foo: 42});
+});
+
+test('loadContext: should load promises with context array', async t => {
+  const promise = Promise.resolve(42);
+  const context = await p.loadContext([{name: 'meaning', value: promise}]);
+  t.deepEqual(context, {meaning: 42});
+});
+
+test('loadContext: should load promises with context object', async t => {
+  const promise = Promise.resolve(42);
+  const context = await p.loadContext({meaning: promise});
+  t.deepEqual(context, {meaning: 42});
+});
+
+test('loadContext: should handle rejected promises', async t => {
+  const promise = new Promise((resolve, reject) => {
+    reject(new Error('ERROR'));
   });
+  const err = await t.throws(p.loadContext({err: promise}));
+  t.is(err.message, 'ERROR');
+});
+
+test('loadConfiguration: should load config from package.json', async t => {
+  const {context, prompt, bannerFunc} = await p.loadConfiguration({
+    package: path.join(__dirname, 'pkg.json'),
+    replrc: false,
+  });
+  t.deepEqual(context, {bar: 42});
+  t.is(prompt, '<TEST> $');
+  t.true(typeof bannerFunc === 'function');
+});
+
+test('loadConfiguration: should allow "repl" config to be an array in package.json', async t => {
+  const {context, prompt, bannerFunc} = await p.loadConfiguration({
+    package: path.join(__dirname, 'pkg-with-repl-array.json'),
+    replrc: false,
+  });
+  t.is(context.bar, 42);
+  t.is(prompt, '[foo] > ');
+  t.true(typeof bannerFunc === 'function');
+  t.true(typeof context.lodash === 'function');
+});
+
+test('loadConfiguration: should allow "repl" config to be an array in replrc', async t => {
+  const {context} = await p.loadConfiguration({
+    package: false,
+    replrc: path.join(__dirname, 'replrc-with-array.js'),
+  });
+  t.is(context.bar, 43);
+  t.true(typeof context.lodash === 'function');
+});
+
+test('loadConfiguration: should load config from a replrc file', async t => {
+  const {context, prompt} = await p.loadConfiguration({
+    package: false,
+    replrc: path.join(__dirname, 'replrc.js'),
+  });
+  t.is(context.bar, 43);
+  t.true(typeof context.lodash === 'function');
+  t.is(prompt, '[TEST] $');
+});
+
+test('loadConfiguration: should give precedence to replrc file', async t => {
+  const {context, bannerFunc} = await p.loadConfiguration({
+    package: path.join(__dirname, 'pkg.json'),
+    replrc: path.join(__dirname, 'replrc.js'),
+  });
+  t.is(context.bar, 43);
+  t.true(typeof context.lodash === 'function');
+  const banner = bannerFunc();
+  t.is(banner, 'TEST');
+});
+
+test('loadConfiguration: should allow prompts to be defined as a function', async t => {
+  const {context, promptFunc, package: pkg} = await p.loadConfiguration({
+    package: path.join(__dirname, 'pkg.json'),
+    replrc: path.join(__dirname, 'replrc-with-prompt-func.js'),
+  });
+  t.is(context.foo, 'TEST');
+  t.true(typeof promptFunc === 'function');
+  t.is(promptFunc(context, pkg), 'TEST foo > ');
+});
+
+test('loadConfiguration: should allow context to be an object', async t => {
+  const {context} = await p.loadConfiguration({
+    package: path.join(__dirname, 'pkg.json'),
+    replrc: path.join(__dirname, 'replrc-with-context-obj.js'),
+  });
+  t.true(typeof context.l === 'function');
+  t.is(context.meaningOfLife, 42);
+});
+
+test('loadConfiguration: should give precedence to passed options', async t => {
+  const {prompt, banner} = await p.loadConfiguration({
+    package: path.join(__dirname, 'pkg.json'),
+    replrc: path.join(__dirname, 'replrc.js'),
+    prompt: 'override > ',
+    banner: 'OVERRIDE',
+  });
+  t.is(prompt, 'override > ');
+  t.is(banner, 'OVERRIDE');
+});
+
+test('contextKey: should camelcase local paths with dashes', t => {
+  t.is(p.contextKey('./utils/foo-bar'), 'fooBar');
+  t.is(p.contextKey('/utils/foo-bar'), 'fooBar');
+});
+
+test('contextKey: should camelcase snakecase names', t => {
+  t.is(p.contextKey('./utils/foo_bar'), 'fooBar');
+});
+
+test('contextKey: should camelcase package names with dashes', t => {
+  t.is(p.contextKey('foo-bar'), 'fooBar');
 });
